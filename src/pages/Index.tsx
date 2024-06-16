0345f8065ae110c17fa43686307e3e08f070e9e1
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-06-16 13:32:44 +0200

    text för SEO

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,10 @@
 import EmailIcon from "@mui/icons-material/Email";
 import Instagram from "@mui/icons-material/Instagram";
 import PhoneIcon from "@mui/icons-material/Phone";
-import { Box, Link, Typography, keyframes } from "@mui/material";
+import { Box, Button, Grid, Link, Typography, keyframes } from "@mui/material";
+import { motion } from "framer-motion";
 import { FormattedMessage } from "react-intl";
+import { useNavigate } from "react-router-dom";
 import Gallery from "../components/Gallery";
 
 // Keyframes for the animation
@@ -18,6 +20,7 @@ const fadeIn = keyframes`
 `;
 
 export default function Index() {
+  const navigate = useNavigate();
   return (
     <Box
       sx={{
@@ -141,21 +144,19 @@ export default function Index() {
               }}
             >
               <Typography
-                variant="h2"
+                component="h2"
                 sx={{
                   color: "white",
                   letterSpacing: 3,
                   marginBottom: { xs: 2, md: 1 },
-                  fontWeight: "400",
+                  fontWeight: "300",
                   fontSize: { xs: 35, md: 50 },
+                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
                 }}
               >
-                <FormattedMessage id="web-applications" />
+                ZEROETT AB
               </Typography>
             </Box>
-            <div
-              style={{ height: 2, width: 200, backgroundColor: "#896daf" }}
-            />
             <Box
               sx={{
                 display: "flex",
@@ -167,21 +168,29 @@ export default function Index() {
               }}
             >
               <Typography
+                component={"h3"}
                 sx={{
                   color: "white",
                   letterSpacing: 2,
                   fontWeight: "300",
-                  fontSize: 16,
+                  fontSize: 20,
                   marginTop: 1,
+                  maxWidth: { xs: "auto", md: "80%" },
+                  textShadow: "1px 1px 3px rgba(0, 0, 0, 0.3)",
                 }}
               >
-                <FormattedMessage id="we-design" />
+                Zeroett AB erbjuder mjukvarulösningar, skräddarsydda hemsidor,
+                e-tjänster, mobilappar, backend-utveckling och databashantering.
               </Typography>
             </Box>
           </Box>
 
           <Box
-            sx={{ display: "flex", flexDirection: "column", marginBottom: 3 }}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              marginBottom: 3,
+            }}
           >
             <Box
               sx={{
@@ -202,9 +211,10 @@ export default function Index() {
                   marginBottom: { xs: 2, md: 1 },
                   fontWeight: "400",
                   fontSize: { xs: 35, md: 50 },
+                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
                 }}
               >
-                <FormattedMessage id="mobile-applications" />
+                <FormattedMessage id="web-applications" />
               </Typography>
             </Box>
             <div
@@ -227,6 +237,63 @@ export default function Index() {
                   fontWeight: "300",
                   fontSize: 16,
                   marginTop: 1,
+                  textShadow: "1px 1px 3px rgba(0, 0, 0, 0.3)",
+                }}
+              >
+                <FormattedMessage id="we-design" />
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{ display: "flex", flexDirection: "column", marginBottom: 3 }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: 2,
+                animation: `${fadeIn} 1s ease-out`,
+                animationDelay: "0.9s",
+                animationFillMode: "forwards",
+                opacity: 0,
+              }}
+            >
+              <Typography
+                variant="h2"
+                sx={{
+                  color: "white",
+                  letterSpacing: 3,
+                  marginBottom: { xs: 2, md: 1 },
+                  fontWeight: "400",
+                  fontSize: { xs: 35, md: 50 },
+                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+                }}
+              >
+                <FormattedMessage id="mobile-applications" />
+              </Typography>
+            </Box>
+            <div
+              style={{ height: 2, width: 200, backgroundColor: "#896daf" }}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                animation: `${fadeIn} 1s ease-out`,
+                animationDelay: "1.1s",
+                animationFillMode: "forwards",
+                opacity: 0,
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "white",
+                  letterSpacing: 2,
+                  fontWeight: "300",
+                  fontSize: 16,
+                  marginTop: 1,
+                  textShadow: "1px 1px 3px rgba(0, 0, 0, 0.3)",
                 }}
               >
                 <FormattedMessage id="building-apps" />
@@ -241,7 +308,7 @@ export default function Index() {
                 alignItems: "center",
                 marginTop: 2,
                 animation: `${fadeIn} 1s ease-out`,
-                animationDelay: "1s",
+                animationDelay: "1.3s",
                 animationFillMode: "forwards",
                 opacity: 0,
               }}
@@ -254,6 +321,7 @@ export default function Index() {
                   marginBottom: { xs: 2, md: 1 },
                   fontWeight: "400",
                   fontSize: { xs: 35, md: 50 },
+                  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
                 }}
               >
                 <FormattedMessage id="backend-solutions" />
@@ -267,7 +335,7 @@ export default function Index() {
                 display: "flex",
                 alignItems: "center",
                 animation: `${fadeIn} 1s ease-out`,
-                animationDelay: "1.2s",
+                animationDelay: "1.5s",
                 animationFillMode: "forwards",
                 opacity: 0,
               }}
@@ -279,6 +347,7 @@ export default function Index() {
                   fontWeight: "300",
                   fontSize: 16,
                   marginTop: 1,
+                  textShadow: "1px 1px 3px rgba(0, 0, 0, 0.3)",
                 }}
               >
                 <FormattedMessage id="robust-and" />
@@ -287,8 +356,127 @@ export default function Index() {
           </Box>
         </Box>
       </Box>
-      {/* <Info /> */}
+
       <Gallery />
+      <Grid
+        container
+        item
+        xs={10}
+        md={8}
+        lg={6}
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          flexDirection: "row",
+          marginBottom: 4,
+          minWidth: { xs: "100%", md: "90%" },
+          marginLeft: { xs: 0, md: 20 },
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: { xs: "center", md: "start" },
+            flex: 1,
+            width: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+            borderRadius: 8,
+            padding: 4,
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.3)",
+            maxWidth: "100%",
+            margin: "auto",
+            animation: `${fadeIn} 1s ease-out`,
+            animationDelay: "0.5s",
+            animationFillMode: "forwards",
+            opacity: 0,
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              color: "white",
+              letterSpacing: 3,
+              marginBottom: { xs: 2, md: 1 },
+              fontWeight: "300",
+              fontSize: { xs: 35, md: 50 },
+              textAlign: { xs: "center", md: "start" },
+            }}
+          >
+            <FormattedMessage id="tell-us" />
+          </Typography>
+          <div
+            style={{
+              height: 2,
+              width: 250,
+              backgroundColor: "#896daf",
+              marginBottom: 1,
+            }}
+          />
+          <Typography
+            variant="body1"
+            sx={{
+              color: "white",
+              letterSpacing: 2,
+              marginY: { xs: 2, md: 1 },
+              fontWeight: "300",
+              textAlign: { xs: "center", md: "start" },
+            }}
+          >
+            <FormattedMessage id="we-assist" />
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              color: "white",
+              letterSpacing: 2,
+              marginBottom: 1,
+              fontWeight: "300",
+              textAlign: { xs: "center", md: "start" },
+            }}
+          >
+            <FormattedMessage id="tell-us-2" />
+          </Typography>
+          <Button
+            onClick={() => {
+              navigate("/contact");
+            }}
+            variant="contained"
+            sx={{
+              backgroundColor: "#662c9c",
+              "&:hover": {
+                backgroundColor: "#422a75",
+              },
+            }}
+          >
+            Kontakt
+          </Button>
+        </Grid>
+
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            justifyContent: { xs: "center", md: "start" },
+            alignItems: { xs: "center", md: "start" },
+          }}
+        >
+          <motion.img
+            src="https://i.imgur.com/LWnnKCt.png"
+            alt="lightbulb"
+            height="300px"
+            width="300px"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 }
