72ca8e4a8409a79a391c13fce70ab7cd3f628fbd
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-07-31 21:19:18 +0200

    comment out index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,29 +1,29 @@
-import * as functions from "firebase-functions";
-import axios from "axios";
+// import * as functions from "firebase-functions";
+// import axios from "axios";
 
-export const createKlarnaOrder = functions.https.onRequest(async (req, res) => {
-  const klarnaApiUrl = "https://api.playground.klarna.com/checkout/v3/orders";
-  const klarnaPublicKey = "your-public-key";
-  const klarnaSecretKey = "your-secret-key";
+// export const createKlarnaOrder = functions.https.onRequest(async (req, res) => {
+//   const klarnaApiUrl = "https://api.playground.klarna.com/checkout/v3/orders";
+//   const klarnaPublicKey = "your-public-key";
+//   const klarnaSecretKey = "your-secret-key";
 
-  try {
-    const response = await axios.post(klarnaApiUrl, req.body, {
-      auth: {
-        username: klarnaPublicKey,
-        password: klarnaSecretKey,
-      },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      res
-        .status(500)
-        .send(error.response ? error.response.data : error.message);
-    } else {
-      res.status(500).send("An unexpected error occurred.");
-    }
-  }
-});
+//   try {
+//     const response = await axios.post(klarnaApiUrl, req.body, {
+//       auth: {
+//         username: klarnaPublicKey,
+//         password: klarnaSecretKey,
+//       },
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       res
+//         .status(500)
+//         .send(error.response ? error.response.data : error.message);
+//     } else {
+//       res.status(500).send("An unexpected error occurred.");
+//     }
+//   }
+// });
