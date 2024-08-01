746e2621568b3de67a4cb4d8011797bad7f5bfa4
Elina Kerola <elinak90@icloud.com>
2024-08-01 10:56:45 +0200

    fix med header logga och bytt plats på ikoner

--- src/components/ZeroettPresentation.tsx
+++ src/components/ZeroettPresentation.tsx
@@ -3,6 +3,7 @@ import { motion, useAnimation } from "framer-motion";
 import { useEffect, useRef } from "react";
 import { FormattedMessage } from "react-intl";
 // import LetsTalk from "./LetsTalk";
+import LetsTalkPhone from "./LetsTalkPhone";
 import { useScreenSize } from "../contexts/screenSizeContext";
 
 export default function ZeroettPresentation() {
@@ -112,10 +113,12 @@ export default function ZeroettPresentation() {
             
             }}
           >
+             < LetsTalkPhone />
             <FormattedMessage id="zeroett-offers" />
           </Typography>
         </Box>
         </Box>
+       
       {/* </motion.div> */}
     </Box>
   );
