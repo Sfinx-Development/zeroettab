062cf4d599262735ea525bf134f6221a190bb579
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-08-19 20:04:50 +0200

    types for payment statuses

--- src/api/SWEDBANKpaymentOrder.ts
+++ src/api/SWEDBANKpaymentOrder.ts
@@ -38,7 +38,109 @@ export async function PostPaymentOrder(paymentOrder: PaymentOrderOutgoing) {
     });
 }
 
-export async function GetPaymentValidation(paidUrl: string) {
+export async function GetPaymentPaidValidation(paidUrl: string) {
+  const uri = paidUrl;
+  const bearer = import.meta.env.VITE_SWEDBANK_BEARER;
+
+  // const sessionId = import.meta.env.VITE_SWEDBANK_SESSIONID;
+  console.log(bearer);
+  return fetch(uri, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;version=3.1",
+      Authorization: `Bearer ${bearer}`,
+      // "User-Agent": "swedbankpay-sdk-dotnet/3.0.1",
+      // Accept: "application/problem+json; q=1.0, application/json; q=0.9",
+      // "Session-Id": sessionId,
+      // Forwarded: "for=192.168.1.157; host=https://localhost:5173; proto=https",
+      Host: "api.externalintegration.payex.com",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Nätverksfel - ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("DATA", data);
+      return data as PaymentInfo;
+    })
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+}
+
+export async function GetPaymentFailedValidation(paidUrl: string) {
+  const uri = paidUrl;
+  const bearer = import.meta.env.VITE_SWEDBANK_BEARER;
+
+  // const sessionId = import.meta.env.VITE_SWEDBANK_SESSIONID;
+  console.log(bearer);
+  return fetch(uri, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;version=3.1",
+      Authorization: `Bearer ${bearer}`,
+      // "User-Agent": "swedbankpay-sdk-dotnet/3.0.1",
+      // Accept: "application/problem+json; q=1.0, application/json; q=0.9",
+      // "Session-Id": sessionId,
+      // Forwarded: "for=192.168.1.157; host=https://localhost:5173; proto=https",
+      Host: "api.externalintegration.payex.com",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Nätverksfel - ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("DATA", data);
+      return data as PaymentInfo;
+    })
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+}
+
+export async function GetPaymentCancelledValidation(paidUrl: string) {
+  const uri = paidUrl;
+  const bearer = import.meta.env.VITE_SWEDBANK_BEARER;
+
+  // const sessionId = import.meta.env.VITE_SWEDBANK_SESSIONID;
+  console.log(bearer);
+  return fetch(uri, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;version=3.1",
+      Authorization: `Bearer ${bearer}`,
+      // "User-Agent": "swedbankpay-sdk-dotnet/3.0.1",
+      // Accept: "application/problem+json; q=1.0, application/json; q=0.9",
+      // "Session-Id": sessionId,
+      // Forwarded: "for=192.168.1.157; host=https://localhost:5173; proto=https",
+      Host: "api.externalintegration.payex.com",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Nätverksfel - ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("DATA", data);
+      return data as PaymentInfo;
+    })
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
+}
+
+export async function GetPaymentAbortedValidation(paidUrl: string) {
   const uri = paidUrl;
   const bearer = import.meta.env.VITE_SWEDBANK_BEARER;
 
--- swedbankTypes.ts
+++ swedbankTypes.ts
@@ -21,3 +21,51 @@ export interface PaymentOrder {
     orderReference: string;
   };
 }
+//TYPER FÖR STATUSAR PÅ BETALNING, ABORTED, CANCELLED, FAILED:
+export interface PaymentAborted {
+  id: string;
+  abortReason: string;
+}
+export interface CancelledPaymentToken {
+  type: string;
+  token: string;
+  name: string;
+  expiryDate: string;
+}
+
+export interface CancelledPaymentDetails {
+  nonPaymentToken: string;
+  externalNonPaymentToken: string;
+}
+
+export interface PaymentCancelled {
+  id: string;
+  cancelReason: string;
+  instrument: string;
+  number: number;
+  payeeReference: string;
+  orderReference: string;
+  transactionType: string;
+  amount: number;
+  submittedAmount: number;
+  feeAmount: number;
+  discountAmount: number;
+  tokens: CancelledPaymentToken[];
+  details: CancelledPaymentDetails;
+}
+
+export interface FailedPaymentProblem {
+  name: string;
+  description: string;
+}
+export interface FailedPaymentProblem {
+  type: string;
+  title: string;
+  status: number;
+  detail: string;
+  problems: FailedPaymentProblem[];
+}
+export interface PaymentFailed {
+  id: string;
+  problem: FailedPaymentProblem;
+}
