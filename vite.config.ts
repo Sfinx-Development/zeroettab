d88d5e954ba3006db6f331cd06f32179886b56d8
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-08-29 18:30:32 +0200

    reverrt vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import react from "@vitejs/plugin-react-swc";
+import fs from "fs";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost.key")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.cert")),
+    },
     proxy: {
       "/psp": {
         target: "https://api.externalintegration.payex.com",
         changeOrigin: true,
-        secure: true, // Ställ in detta på false om du använder självsignerade certifikat
+        secure: false, // Ställ in detta på false om du använder självsignerade certifikat
       },
     },
   },
