d01d0c70bcbd09cf309fb957228a5521400b814d
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-08-29 17:44:39 +0200

    testar utan cert i config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
-import fs from "fs";
-import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.cert")),
-    },
     proxy: {
       "/psp": {
         target: "https://api.externalintegration.payex.com",
