c45e7e4e8db0b5d93d11cd412e9028af3ac6434b
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-11-04 20:48:07 +0100

    c

--- src/components/parallax/ParallaxContact.tsx
+++ src/components/parallax/ParallaxContact.tsx
@@ -4,26 +4,24 @@ import { Customer, useCustomerContext } from "../../context/customerContext";
 import { Rubrik } from "../Footer";
 
 export default function ParallaxContact() {
-  const { customer } = useCustomerContext();
+  const { customer, setCustomer } = useCustomerContext();
+
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  const [snackbarMessage, setSnackbarMessage] = React.useState("");
+  const [snackbarSeverity, setSnackbarSeverity] =
+    React.useState<AlertColor>("success");
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
   const sendEmailWithLink = (customer: Customer) => {
     const customerName = `Kundens namn : ${customer.name}`;
     const customerEmail = `Kundens email : ${customer.email}`;
     const customerPhone = `Kundens email : ${customer.phone}`;
-    const customerAddress = `Kundens adress: ${customer.address} ${customer.zipcode} ${customer.city}`;
-    const customerPurpose = `Syfte med applikationen: ${customer.purposeApp}`;
-    const customerApp = `Typ av appplikation: ${customer.typeOfApp}`;
-    const customerTargetGroup = `Målgrupp som vi riktar in oss på: ${customer.targetGroup}`;
-    const customerBudget = `Budget: ${customer.budgetDescription}`;
-    const customerExtra = `Övrigt: ${customer.extraDescription}`;
+    const customerExtra = `Beskrivning: ${customer.extraDescription}`;
 
-    const isCompany = `Är företag: ${customer.isCompany ? "ja" : "nej"}`;
-    let companyName = "";
-    let companyDescription = "";
-    if (isCompany) {
-      companyName = `Företagets namn: ${customer.companyName}`;
-      companyDescription = `Företagets beskrivning: ${customer.companyDescription}`;
-    }
-    const body = `${customerName}\n${customerEmail}\n${customerPhone}\n${customerAddress}\n${customerPurpose}\n${customerTargetGroup}\n${customerApp}\n${customerBudget}\n${customerExtra}\n${isCompany}\n${companyName}\n${companyDescription}`;
+    const body = `${customerName}\n${customerEmail}\n${customerPhone}\n${customerExtra}`;
 
     const templateParams = {
       to_name: "Zeroett",
@@ -48,7 +46,6 @@ export default function ParallaxContact() {
         setOpenSnackbar(true);
       });
   };
-
   const StyledTextField = styled(TextField)({
     "& .MuiInputBase-root": {
       background: "linear-gradient(to right,#E6A08E, #F1B8A9)",
