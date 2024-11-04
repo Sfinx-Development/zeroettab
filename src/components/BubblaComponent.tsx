fddbfcc80f42c663bfd507ed12d4e5b7c24c41d2
Elina Kerola <elinak90@icloud.com>
2024-11-04 14:21:30 +0100

    bubble commit

--- src/components/BubblaComponent.tsx
+++ src/components/BubblaComponent.tsx
@@ -33,13 +33,13 @@ export default function BubblaComponent() {
         <Box
           sx={{
             position: "relative",
-            width: "80%",
-            height: "200px",
+            width: { xs: "90%", sm: "80%", md: "60%" }, // Responsiv bredd
+            height: { xs: "200px", md: "200px", sm: "280", lg: "200", xl: "300", }, // Responsiv höjd
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            padding: 2, // Minskat padding
-            marginBottom: -10, // Negativ margin för att skapa överlapp nedåt
+            padding: { xs: 1, md: 2 }, // Anpassad padding
+            marginBottom: { xs: -6, md: -10 }, // Negativ margin för överlapp
             textAlign: "center",
             flexDirection: "column",
           }}
@@ -50,13 +50,13 @@ export default function BubblaComponent() {
             sx={{
               color: "rgb(216,163,153)",
               marginBottom: 1,
-              fontSize: 22,
+              fontSize: { xs: 18, md: 18, sm: 22, lg: 22 }, 
               fontWeight: 600,
             }}
           >
             Frontend
           </Rubrik>
-          <Typography sx={{ color: "#5B5B5B", maxWidth: "35%", fontSize: 14 }}>
+          <Typography sx={{ color: "#5B5B5B", maxWidth: "50%", fontSize: { xs: 12, md: 14 } }}>
             Vi designar och bygger gränssnitt utifrån dina personliga önskemål
             samtidigt som vi säkerställer användarvänligheten.
           </Typography>
@@ -74,14 +74,14 @@ export default function BubblaComponent() {
         <Box
           sx={{
             position: "relative",
-            width: "80%",
-            height: "200px",
+            width: { xs: "90%", sm: "80%", md: "60%" },
+            height: { xs: "250px", md: "250px", sm: "280", lg: "200", xl: "300", }, // Responsiv höjd
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
-            padding: 2, // Minskat padding
-            marginBottom: -10, // Negativ margin för att skapa överlapp nedåt
+            padding: { xs: 1, md: 2 },
+            marginBottom: { xs: -6, md: -10 },
             textAlign: "center",
           }}
         >
@@ -91,13 +91,13 @@ export default function BubblaComponent() {
             sx={{
               color: "#DBA569",
               marginBottom: 1,
-              fontSize: 22,
+              fontSize: { xs: 18, md: 22 },
               fontWeight: 600,
             }}
           >
             Backend
           </Rubrik>
-          <Typography sx={{ color: "#5B5B5B", maxWidth: "30%", fontSize: 14 }}>
+          <Typography sx={{ color: "#5B5B5B", maxWidth: "50%", fontSize: { xs: 12, md: 14 }  }}>
             Vi utvecklar kraftfulla backend-lösningar, inklusive system och
             API:er som säkerställer att vår mjukvara fungerar smidigt och kan
             kommunicera med andra system.
@@ -116,13 +116,13 @@ export default function BubblaComponent() {
         <Box
           sx={{
             position: "relative",
-            width: "80%",
-            height: "200px",
+            width: { xs: "90%", sm: "80%", md: "60%" },
+            height: { xs: "200px", md: "200px", sm: "280", lg: "200", xl: "300", }, // Responsiv höjd
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             flexDirection: "column",
-            padding: 2, // Minskat padding
+            padding: { xs: 1, md: 2 },
             textAlign: "center",
           }}
         >
@@ -131,13 +131,13 @@ export default function BubblaComponent() {
             sx={{
               color: "rgba(34,32,37,255)",
               marginBottom: 1,
-              fontSize: 22,
+              fontSize: { xs: 18, md: 22 },
               fontWeight: 600,
             }}
           >
             Fullstack
           </Rubrik>
-          <Typography sx={{ color: "#5B5B5B", maxWidth: "35%", fontSize: 14 }}>
+          <Typography sx={{ color: "#5B5B5B", maxWidth: "50%", fontSize: { xs: 12, md: 14 } }}>
             Vi bygger användargränssnitt utifrån dina personliga önskemål,
             samtidigt som vi säkerställer användarvänligheten.
           </Typography>
